0d68898be7e81474eba3d4053a5676880bb1247d
akshit sanwal <asancool007@gmail.com>
2021-09-21 12:15:45 +0530

    3rd algo

---
+++ dijkstraAlgo.java
@@ -0,0 +1,77 @@
+
+/* SAME AS BFS BUT WE USE PRIORITY QUEUE INSTEAD OF QUEUE SO IT WILL REMOVE AUTOMATICALLY THE LOW PRIORITY ELEMENT OR LESS WT ELEMENT 
+SO THE DIFFRENCE BTW BFS AND DIKJISTRA IS CLEARLY VISIBLE HERE AND THATS WHY BFS CALCULTE SHORTEST PATH IN TERMS OF EDGES AND DIJKSTRA CALCULATE SHORTEST PATH IN TERMS OF EDGE WEIGHT ,,, ALSO BFS GROW IN RADIUS I.E. FIRST ALL THE PATH WITH LENGTH ONE FROM SOURCE TO DESTINATION AND THEN LENGTH 2 AND SO ON */
+
+import java.io.*;
+import java.util.*;
+
+public class dijkstraAlgo {
+      static class Edge {
+            int src;
+            int nbr;
+            int wt;
+
+            Edge(int src, int nbr, int wt) {
+                  this.src = src;
+                  this.nbr = nbr;
+                  this.wt = wt;
+            }
+      }
+
+      public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int vtces = Integer.parseInt(br.readLine());
+            ArrayList<Edge>[] graph = new ArrayList[vtces];
+            for (int i = 0; i < vtces; i++) {
+                  graph[i] = new ArrayList<>();
+            }
+
+            int edges = Integer.parseInt(br.readLine());
+            for (int i = 0; i < edges; i++) {
+                  String[] parts = br.readLine().split(" ");
+                  int v1 = Integer.parseInt(parts[0]);
+                  int v2 = Integer.parseInt(parts[1]);
+                  int wt = Integer.parseInt(parts[2]);
+                  graph[v1].add(new Edge(v1, v2, wt));
+                  graph[v2].add(new Edge(v2, v1, wt));
+            }
+
+            int src = Integer.parseInt(br.readLine());
+
+            PriorityQueue<Pair> queue = new PriorityQueue<>();
+            queue.add(new Pair(src, src + "", 0));
+            boolean[] visited = new boolean[vtces];
+            while (queue.size() > 0) {
+                  Pair rem = queue.remove();
+
+                  if (visited[rem.v] == true) {
+                        continue;
+                  }
+                  visited[rem.v] = true;
+                  System.out.println(rem.v + " via " + rem.psf + " @ " + rem.wsf);
+
+                  for (Edge e : graph[rem.v]) {
+                        if (visited[e.nbr] == false) {
+                              queue.add(new Pair(e.nbr, rem.psf + e.nbr, rem.wsf + e.wt));
+                        }
+                  }
+            }
+      }
+
+      static class Pair implements Comparable<Pair> {
+            int v;
+            String psf;
+            int wsf;
+
+            Pair(int v, String psf, int wsf) {
+                  this.v = v;
+                  this.psf = psf;
+                  this.wsf = wsf;
+            }
+
+            public int compareTo(Pair o) {
+                  return this.wsf - o.wsf;
+            }
+      }
+}
